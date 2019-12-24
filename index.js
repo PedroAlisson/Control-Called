c8e6b08a330b8dc9e469b9de2ccb498e5da78064
PedroAlisson <PedroAlisson@users.noreply.github.com>
2019-12-24 14:42:45 -0300

    Commit Routers ingles

--- index.js
+++ index.js
@@ -44,55 +44,54 @@ app.get('/user/dashboard', (request,response)=>{
 })
 
 
-app.get('/user/chamados/new', async(request,response)=>{
-    response.render('user/novo')
+app.get('/user/called/new', async(request,response)=>{
+    response.render('user/called_new')
 })
 
-app.post('/user/chamados/new', async(request,response)=>{
+app.post('/user/called/new', async(request,response)=>{
     const {email,description,status} = request.body
     const db = await dbConnection
     await db.run(`insert into called (email, description, status) values('${email}','${description}','${status}');`)
-    response.render('user/novo')
+    response.render('user/called_new')
 })
 
-app.post('/user/chamados/edit/:id', async(request,response)=>{
+app.post('/user/called/edit/:id', async(request,response)=>{
     const {email,description,status} = request.body
     const {id} = request.params 
     const db = await dbConnection
     await db.run(`update called set email ='${email}', description ='${description}' , status='${status}' where id = ${id};`)
-    response.render('user/novo')
+    response.render('user/called_new')
 }) 
 
-app.get('/user/chamados/edit/:id', async(request,response)=>{
+app.get('/user/called/edit/:id', async(request,response)=>{
     const db = await dbConnection
     const calledsDB = await db.get('select * from called where id ='+request.params.id)
-    response.render('user/edit_chamados')
+    response.render('user/edit_called')
 })
 
-app.get('/user/chamados/delete/:id', async(request,response)=>{
+app.get('/user/called/delete/:id', async(request,response)=>{
     const db = await dbConnection
     await db.run('delete from called where id ='+request.params.id)
     //response.render('/delete') Apagado
 })
 
-app.get('/user/chamados', async(request,response)=>{   
+app.get('/user/called', async(request,response)=>{   
     const db = await dbConnection
     const calleds = await db.all('select * from called;')
-    response.render('user/chamados',{
+    response.render('user/called',{
         calleds
     })
 })
 
-app.get('/user/consulta', async(request,response)=>{
-
-    response.render('user/consulta')
+app.get('/user/query', async(request,response)=>{
+    response.render('user/query')
 })
 
-app.post('/user/consulta', async(request,response)=>{
+app.post('/user/query', async(request,response)=>{
     const {number} = request.body
     const db = await dbConnection
     const calleds = await db.get(`select * from called where id ='${number}';`)
-    response.render('user/result_consulta',{
+    response.render('user/result_query',{
          calleds 
     })
 })
@@ -134,7 +133,7 @@ app.post('/admin/userlist/edit/:id', async(request,response)=>{
     const {id} = request.params 
     const db = await dbConnection
     await db.run(`update users set name ='${name}', empresa ='${empresa}', email ='${email}' , status ='${status}', password ='${password}', type ='${type}' where id = ${id};`)
-   // response.render('user/novo')
+   // response.render('user/registration_called')
 }) 
 
 app.get('/admin/userlist/edit/:id', async(request,response)=>{
@@ -149,49 +148,49 @@ app.get('/admin/userlist/delete/:id', async(request,response)=>{
    // response.render('admin/delete')
 })
 
-app.get('/admin/consulta', async(request,response)=>{
-    response.render('admin/consulta_usuario')
+app.get('/admin/query', async(request,response)=>{
+    response.render('admin/query_usuario')
 })
 
-app.post('/admin/consulta', async(request,response)=>{
+app.post('/admin/query', async(request,response)=>{
     const {name} = request.body
     const db = await dbConnection
     const users = await db.get(`select * from users where name ='${name}';`)
-    response.render('admin/result_consulta_usuario',{
+    response.render('admin/result_query_usuario',{
          users 
     })
 })
 
-app.get('/admin/chamados', async(request,response)=>{
+app.get('/admin/called', async(request,response)=>{
     const db = await dbConnection
     const calleds = await db.all('select * from called;')
-    response.render('admin/chamados',{
+    response.render('admin/called',{
         calleds
     }) 
 })
 
-app.get('/admin/consulta/chamados', (request,response)=>{   
-    response.render('admin/consulta_usuario')
+app.get('/admin/query/called', (request,response)=>{   
+    response.render('admin/query_called')
 })
 
-app.post('/admin/consulta/chamados', async(request,response)=>{   
+app.post('/admin/query/called', async(request,response)=>{   
     const {number} = request.body
     const db = await dbConnection
     const calleds = await db.get(`select * from called where id ='${number}';`)
-    response.render('admin/resul_consulta_chamados',{
+    response.render('admin/result_query_called',{
         calleds
     })
 })
 
-app.get('/admin/chamados/new', async(request,response)=>{
-    response.render('admin/novo')
+app.get('/admin/called/new', async(request,response)=>{
+    response.render('admin/registration_called')
 })
 
-app.post('/admin/chamados/new', async(request,response)=>{
+app.post('/admin/called/new', async(request,response)=>{
     const {email,description,status} = request.body
     const db = await dbConnection
     await db.run(`insert into called (email, description, status) values('${email}','${description}','${status}');`)
-    response.render('admin/novo')
+    response.render('admin/registration_called')
 })
 
 
@@ -202,7 +201,6 @@ await db.run('create table if not exists called (id INTEGER PRIMARY KEY, users_i
 
 //await db.run('drop table called;')
 //await db.run('drop table users;')
-
 
 }
 
